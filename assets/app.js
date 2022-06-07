492846f54d2e476ca01677f9b20bb1243bc20814
Sifiso <sifiso@mfundopedia>
2022-06-07 20:37:20 +0200

    wip: new nctvet college website project

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import './styles/style.css';
+import './styles/font-awesome/all.css';
+import './styles/font-awesome/all.min.css';
+import './styles/animate/animate.css'
+import './styles/animate/animate.min.css'
+import './styles/bootstrap/bootstrap.css'
+import './styles/bootstrap/bootstrap.min.css'
+import './styles/datetimepicker/datetimepicker.min.css'
+import './styles/flaticon/flaticon.css'
+import './styles/magnific-popup/magnific-popup.css'
+import './styles/owl-carousel/owl.carousel.css'
+import './styles/owl-carousel/owl.carousel.min.css'
+import './styles/select2/select2.css'
+import './styles/select2/select2.min.css'
+import './styles/slick/slick-theme.css'
+import './styles/style.css.map'
+import './styles/swiper/swiper.css'
+import './styles/swiper/swiper.min.css'
+
+// start the Stimulus application
+import './bootstrap';
